dc98149fa53f8865e24342856a3dba6eab1b2d08
Jose Costa Teixeira <jose.a.teixeira@gmail.com>
2023-01-19 00:00:46 +0100

    hl7-eu

---
+++ hl7-eu/publish.cmd
@@ -0,0 +1,1 @@
+java -jar publisher.jar -Dfile.encoding=UTF-8 -go-publish -source pcsp -web %CD%/webroot -temp %CD%/temp -registry ../ig-registry/fhir-ig-list.json -history ../ig-history -templates %CD%/webroot/templates
--- hl7-eu/publishhl7eu.cmd
+++
@@ -1,1 +0,0 @@
-java -jar publisher.jar -Dfile.encoding=UTF-8 -go-publish -source pcsp -web %CD%/web-root -temp %CD%/temp -registry ../ig-registry/fhir-ig-list.json -history ../ig-history -templates %CD%/web-root/templates
