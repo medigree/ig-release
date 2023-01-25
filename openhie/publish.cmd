688a2503efee0e1d7a2330022def696cda66cb14
Jose Costa Teixeira <jose.a.teixeira@gmail.com>
2023-01-25 21:33:09 +0100

    update ohie

--- openhie/publish.cmd
+++ openhie/publish.cmd
@@ -1,1 +1,3 @@
-java -jar publisher.jar -Dfile.encoding=UTF-8 -go-publish -source case-reporting -web %CD%/webroot -temp %CD%/temp -registry ../ig-registry/fhir-ig-list.json -history ../ig-history -templates %CD%/webroot/templates
+java -Dfile.encoding=UTF-8 -jar publisher.jar -go-publish -source case-reporting -web %CD%/webroot -temp %CD%/temp -registry ../ig-registry/fhir-ig-list.json -history ../ig-history -templates %CD%/webroot/templates
+
+
