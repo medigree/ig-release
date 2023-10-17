dea7e5af6b150a1d5ed003328089c77516eeb211
Jose Costa Teixeira <jose.a.teixeira@gmail.com>
2023-10-17 14:55:59 +0200

    add eu lab

--- hl7-eu/publish.cmd
+++ hl7-eu/publish.cmd
@@ -1,1 +1,3 @@
-java -Dfile.encoding=UTF-8 -jar publisher.jar -go-publish -source pcsp -web %CD%/webroot -temp %CD%/temp -registry ../ig-registry/fhir-ig-list.json -history ../ig-history -templates %CD%/webroot/templates
+
+java -Dfile.encoding=UTF-8 -jar publisher.jar -generate-package-registry %CD%/webroot
+java -Dfile.encoding=UTF-8 -jar publisher.jar -go-publish -source laboratory -web %CD%/webroot -temp %CD%/temp -registry ../ig-registry/fhir-ig-list.json -history ../ig-history -templates %CD%/webroot/templates
